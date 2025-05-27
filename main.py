f71bb1ba573f5693e4f69432d2845bc1dd9cfd90
Areeb Ahmed <areeb.ahmed@ephlux.com>
2025-05-27 12:39:54 +0500

    add origin

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app = FastAPI(title="Travel Planner API", description="API for generating travel
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Add your frontend URL
+    allow_origins=[ "http://localhost:3000",
+        "https://pakigentravel.vercel.app"],  # Add your frontend URL
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
